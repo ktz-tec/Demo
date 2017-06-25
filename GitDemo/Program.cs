736657c58d239d5ab6ac5bb250934f2a417e5ae6
wangyuyincnx <wang2238267@163.com>
2017-06-25 10:18:39 +0800

    追加测试

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -16,6 +16,8 @@ namespace GitDemo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //test 123456
         }
     }
 }
